bbe9a4edb68912dfef9b86fe7ef0f56684516938
kakiba <97882386+kotto5@users.noreply.github.com>
2024-01-02 15:26:13 +0900

    create base structure (#3)

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,3 +1,42 @@
+#include <vector>
+#include <exception>
+#include <iostream>
+
+std::vector<std::vector<double> > input(void) {
+    std::vector<std::vector<double> > v;
+    return v;
+}
+
+std::vector<std::vector<double> > reverse(std::vector<std::vector<double> > v) {
+    return v;
+}
+
+std::vector<std::vector<double> > FloydWarshall(std::vector<std::vector<double> > v) {
+    return v;
+}
+
+std::vector<unsigned int> getShortestPath(std::vector<std::vector<double> > v) {
+    std::vector<unsigned int> shortestPath;
+    return shortestPath;
+}
+
+
 int main() {
+    std::vector<std::vector<double> > distances;
+    try {
+        distances = input();
+    }
+    catch (std::exception e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    const std::vector<std::vector<double> > reverseDistances = reverse(distances);
+    const std::vector<std::vector<double> > shortestDistances = FloydWarshall(reverseDistances);
+    const std::vector<unsigned int> shortestPath = getShortestPath(shortestDistances);
+    for (auto itr = shortestPath.begin(), end = shortestPath.end();
+        itr != end; ++itr) {
+        std::cout << *itr << "\r\n";
+    }
+    
     return 0;
 }
